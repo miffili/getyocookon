c5854d0d07e85e1f0ef3317b13d51a4806dbc5df
Klara <fleischmann.kl@gmail.com>
2018-04-19 01:28:52 +0200

    Fix rating provisionally

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,34 +1,19 @@
 import React from "react";
 import { Button, Thumbnail, Col } from 'react-bootstrap';
-import spatula from '../spatula.png';
-import spatulaFull from '../spatula_full.png';
+// import spatula from '../spatula.png';
+// import spatulaFull from '../spatula_full.png';
 
 const RecipeCard = (props) => {
   const buttonStyle = {
     width: "100%",
     textTransform: "uppercase",
     letterSpacing: "0.2rem"
-  };
+  }
+
   const ratingStyle = {
     display: "flex",
     justifyContent: "start",
     width: "100%"
-  }
-
-  const fullSpatula = {
-    width: "3rem",
-    height: "3rem",
-    marginRight: "0.5rem",
-    backgroundImage: `url('${spatulaFull}')`,
-    backgroundSize: "contain"
-  }
-
-  const emptySpatula = {
-    width: "3rem",
-    height: "3rem",
-    marginRight: "0.5rem",
-    backgroundSize: "contain",
-    backgroundImage: `url('${spatula}')`
   }
 
   return (
@@ -39,14 +24,10 @@ const RecipeCard = (props) => {
         className="thumbnail"
       >
         <div style={ratingStyle}>
-          <div style={fullSpatula} />
-          <div style={fullSpatula} />
-          <div style={fullSpatula} />
-          <div style={fullSpatula} />
-          <div style={emptySpatula} />
-            </div>
-            <h3>Pizza Vegetaria</h3>
-            <p>
+          ⭐⭐⭐⭐⭐
+        </div>
+        <h3>Delicious Dish</h3>
+        <p>
           <Button bsStyle="default" style={buttonStyle}>more...</Button>
         </p>
       </Thumbnail>
