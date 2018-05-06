885ae26a67cf0703369e62f81b7a7a51b341f591
Klara <fleischmann.kl@gmail.com>
2018-05-06 22:40:17 +0200

    Change RecipeCard from functional to class component in preparation of modal components

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -13,64 +13,63 @@ import {
   Col
 } from "reactstrap";
 
-const RecipeCard = props => {
-  // console.log(props)
-  const buttonStyle = {
-    width: "100%",
-    textTransform: "uppercase",
-    letterSpacing: "0.2rem"
+class RecipeCard extends React.Component {
+  static propTypes = {
+    details: PropTypes.shape({
+      image: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      categories: PropTypes.array
+    })
   };
-  const badgeStyle = {
-    margin: "0.1rem",
-    fontWeight: "inherit",
-    border: "1px solid lightgray",
-    backgroundColor: "rgba(255, 255, 255, 0.8)"
+
+  render() {
+    const buttonStyle = {
+      width: "100%",
+      textTransform: "uppercase",
+      letterSpacing: "0.2rem"
+    };
+    const badgeStyle = {
+      margin: "0.1rem",
+      fontWeight: "inherit",
+      border: "1px solid lightgray",
+      backgroundColor: "rgba(255, 255, 255, 0.8)"
+    };
+
+    //
+    // const ratingStyle = {
+    //   display: "flex",
+    //   justifyContent: "start",
+    //   width: "100%"
+    // };
+    return (
+      <Col xs={12} md={4} xl={3}>
+        <Card style={{ margin: "0.5rem 0" }}>
+          <img
+            width="100%"
+            src={this.props.details.image}
+            alt={this.props.details.title}
+          />
+          <CardBody>
+            <CardTitle>{this.props.details.title}</CardTitle>
+            {/* <CardText>
+              <div style={ratingStyle}>⭐⭐⭐⭐⭐</div>
+              {this.props.details.shortDesc}
+            </CardText> */}
+            <Button outline color="secondary" size="sm" style={buttonStyle}>
+              more...
+            </Button>
+          </CardBody>
+          <CardFooter>
+            {this.props.details.categories.map(cat => (
+              <Badge color="light" style={badgeStyle} key={cat}>
+                {cat}
+              </Badge>
+            ))}
+          </CardFooter>
+        </Card>
+      </Col>
+    );
   }
-
-  //
-  // const ratingStyle = {
-  //   display: "flex",
-  //   justifyContent: "start",
-  //   width: "100%"
-  // };
-
-  return (
-    <Col xs={12} md={4} xl={3}>
-      <Card style={{ margin: "0.5rem 0" }}>
-        <img
-          width="100%"
-          src={props.details.image}
-          alt={props.details.title}
-        />
-        <CardBody>
-          <CardTitle>{props.details.title}</CardTitle>
-          {/* <CardText>
-            <div style={ratingStyle}>⭐⭐⭐⭐⭐</div>
-            {props.details.shortDesc}
-          </CardText> */}
-          <Button outline color="secondary" size="sm" style={buttonStyle}>
-            more...
-          </Button>
-        </CardBody>
-        <CardFooter>
-          {props.details.categories.map(cat =>
-            <Badge
-              color="light"
-              style={badgeStyle}
-              key={cat}>{cat}</Badge>
-          )}
-        </CardFooter>
-      </Card>
-    </Col>
-  );
-};
-
-RecipeCard.propTypes = {
-  details: PropTypes.shape({
-    image: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    categories: PropTypes.array
-  })
 }
 
 export default RecipeCard;
